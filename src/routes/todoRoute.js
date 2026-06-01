9db5f408d56013165788785091628a2b830f64a0
Subhadeep Saha <rajsaha.sep@gmail.com>
2026-06-01 17:05:35 +0530

    validate update

--- src/routes/todoRoute.js
+++ src/routes/todoRoute.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { createTodo, deleteTodo, getAllTodo, updateTodo } from "../controllers/todoController.js";
 import { hasToken } from "../middleware/hasToken.js";
+import { todoValidateSchema, validateTodo } from "../validators/todoValidate.js";
 
 const todoRoute = express.Router();
 
-todoRoute.post("/create",hasToken,createTodo);
-todoRoute.get("/getAll",hasToken,getAllTodo);
-todoRoute.delete("/delete/:id",hasToken,deleteTodo)
-todoRoute.put("/update/:id",hasToken,updateTodo)
+todoRoute.post("/create", hasToken, validateTodo(todoValidateSchema), createTodo);
+todoRoute.get("/getAll", hasToken, getAllTodo);
+todoRoute.delete("/delete/:id", hasToken, deleteTodo)
+todoRoute.put("/update/:id", hasToken, updateTodo)
 
 
 
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -2,12 +2,13 @@ import express from "express"
 import { login, logout, register } from "../controllers/userController.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 import { hasToken } from './../middleware/hasToken.js';
+import { userValidateSchema, validateUser } from "../validators/userValidate.js";
 
 const userRoute = express.Router();
 
-userRoute.post("/register",register);
-userRoute.post("/login",login);
-userRoute.get("/verify",verifyToken);
-userRoute.delete("/logout",hasToken,logout)
+userRoute.post("/register", validateUser(userValidateSchema), register);
+userRoute.post("/login", login);
+userRoute.get("/verify", verifyToken);
+userRoute.delete("/logout", hasToken, logout)
 
 export default userRoute
