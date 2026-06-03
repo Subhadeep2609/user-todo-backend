95c8d5e00e628fca40d27b4577fea2f757e240c8
Subhadeep Saha <rajsaha.sep@gmail.com>
2026-06-03 16:20:37 +0530

    multer added

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ import dotenv from "dotenv/config"
 import dbConnect from "./src/config/dbConnect.js";
 import userRoute from "./src/routes/userRoute.js";
 import todoRoute from "./src/routes/todoRoute.js";
+import multerRoute from "./src/routes/multerRoute.js";
 
 const port = process.env.PORT;
 
 const app = express();
 
 app.use(express.json())
+app.use("/upload", express.static("upload"))
 app.use("/user",userRoute);
-app.use("/todo",todoRoute)
+app.use("/todo",todoRoute);
+app.use("/pic",multerRoute)
+
 dbConnect()
 
 app.listen(port,()=>{
